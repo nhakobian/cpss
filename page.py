a2fcf36739cd42b748a257d11f9a20bde3f38dd4
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2011-09-14 16:16:07 -0500

    Tweaked the page footer.

--- page.py
+++ page.py
@@ -139,7 +139,7 @@ class Page:
         </div>
         <div class="copyright">
         All site content &copy;2005-2011 CARMA, all rights reserved.<br>
-        Site maintained by Nicholas Hakobian &lt;<a href="mailto:proposal-help@astro.illinois.edu">proposal-help@astro.illinois.edu</a>&gt;.
+        Site maintained by the Proposal-Help Team &lt;<a href="mailto:proposal-help@astro.illinois.edu">proposal-help@astro.illinois.edu</a>&gt;.
         </div>
         </body>
         </html>""")
