9681918b42ba4458cc6f51aee0007bd7160a82d3
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2011-09-14 14:32:20 -0500

    Added copyright line

--- page.py
+++ page.py
@@ -137,6 +137,10 @@ class Page:
         self.req.write("""
         </div>
         </div>
+        <div class="copyright">
+        All site content &copy;2005-2011 CARMA, all rights reserved.<br>
+        Site maintained by Nicholas Hakobian &lt;<a href="mailto:proposal-help@astro.illinois.edu">proposal-help@astro.illinois.edu</a>&gt;.
+        </div>
         </body>
         </html>""")
         
