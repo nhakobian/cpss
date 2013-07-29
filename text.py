16ac9bff23ae95f0013eaeeb7132d38879513bcf
Nicholas Szandor Hakobian <nhakobian@gmail.com>
2013-07-29 11:04:07 -0500

    Updated copyright notice to 2013.

--- text.py
+++ text.py
@@ -721,7 +721,7 @@ page_footer=r"""  </div>
 </div>
 
 <div id="copyright" class="nojs">
-  All site content &copy;2005-2012 CARMA, all rights reserved.<br>
+  All site content &copy;2005-2013 CARMA, all rights reserved.<br>
   Site maintained by the Proposal-Help Team 
   &lt;<a href="mailto:proposal-help@astro.illinois.edu">proposal-help@astro.illinois.edu</a>&gt;.
 </div>
--- text/page_footer.tpl
+++ text/page_footer.tpl
@@ -2,7 +2,7 @@
 </div>
 
 <div id="copyright" class="nojs">
-  All site content &copy;2005-2012 CARMA, all rights reserved.<br>
+  All site content &copy;2005-2013 CARMA, all rights reserved.<br>
   Site maintained by the Proposal-Help Team 
   &lt;<a href="mailto:proposal-help@astro.illinois.edu">proposal-help@astro.illinois.edu</a>&gt;.
 </div>
